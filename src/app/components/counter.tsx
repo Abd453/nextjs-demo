d84a238b2ae8fb057b1df2794c67700bb59b691f
Abd453 <134734720+Abd453@users.noreply.github.com>
2025-03-03 23:54:14 +0300

    Final commit

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -3,13 +3,17 @@ import { useState } from 'react';
 import {useAuth, useUser} from '@clerk/nextjs';
 
 export const Counter = () => {
-const {isLoaded, userId, sessionId, getToken} = useAuth();
+//const {isLoaded, userId, sessionId, getToken} = useAuth();
+const {isLoaded, isSignedIn, user } = useUser();
 
     console.log("Counter component");
     const [count, setCount] = useState(0);
 
     
-    if (!isLoaded || !userId){
+    // if (!isLoaded || !userId){
+    //   return null;
+    // }
+    if (!isLoaded || !isSignedIn){
       return null;
     }
   return (
